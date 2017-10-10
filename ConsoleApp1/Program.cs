5958b2c74d687dadf836560d09c495eb9a4f1990
Marco <griuska@hotmail.com>
2017-10-10 06:41:52 +0200

    Esercizio Array 09.10.2017

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WindowWidth = 200;
+            Console.WindowHeight = 10;
+
+            Console.WriteLine("dimmi quanti slots");
+            int slots = int.Parse(Console.ReadLine());
+
+            int[] array1 = new int[slots];
+            int n; int sum = 0;
+            for (n = 0; ;)
+            {
+                if (n < slots)
+                {
+                    Console.WriteLine("slot" + n);
+                    array1[n] = int.Parse(Console.ReadLine());
+                    sum = sum + array1[n];
+                    n++;
+                }
+                else
+                    break;
+
+            }
+
+
+            Console.WriteLine("Adesso facciamo la somma: " + sum);
+
+            Console.ReadLine();
+
+        }
+    }
+}
